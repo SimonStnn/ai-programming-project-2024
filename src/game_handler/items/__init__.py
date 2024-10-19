30f56d89352d22e31333ca930eb6deb3196c5194
SimonStnn <simon.stijnen.23@gmail.com>
2024-10-19 17:18:54 +0200

    Update items imports

--- src/game_handler/items/__init__.py
+++ src/game_handler/items/__init__.py
@@ -1,4 +1,4 @@
 """All items in the game are imported here to be used in the game."""
 from src.game_handler._item import Item, Craftable, Recipe
 from src.game_handler.items.log import Log
-from src.game_handler.items.mossy_log import Mossy_Log
+from src.game_handler.items.stick import Stick
