2d3985d07faad1004bdbb785164e282a1a517c23
SimonStnn <simon.stijnen.23@gmail.com>
2024-10-30 11:18:59 +0100

    Add Item.name tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -16,3 +16,16 @@ class TestItem(unittest.TestCase):
     def test_item_repr(self):
         item = Log()
         self.assertEqual(repr(item), "Log()")
+
+    def test_item_name(self):
+        item = Log()
+        self.assertEqual(item.name, "Log")
+        self.assertEqual(item.name, Log.__name__.replace("_", " "))
+        self.assertEqual(Log.name, "Log")
+        self.assertEqual(Log.name, Log.__name__.replace("_", " "))
+
+        item = Test_Item()
+        self.assertEqual(item.name, "Test Item")
+        self.assertEqual(item.name, Test_Item.__name__.replace("_", " "))
+        self.assertEqual(Test_Item.name, "Test Item")
+        self.assertEqual(Test_Item.name, Test_Item.__name__.replace("_", " "))
