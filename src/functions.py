23f704c354e583aadc8a7816bbded6ca69984b24
SimonStnn <simon.stijnen.23@gmail.com>
2024-11-08 15:36:31 +0100

    Fix keyboard layout bug

--- src/functions.py
+++ src/functions.py
@@ -1,8 +1,8 @@
 import os, platform, sys
+import locale
 
 if platform.system() == "Windows":
     def get_keyboard_layout():
-        import locale
         current_locale = locale.getdefaultlocale()[0]
         azerty_locales = ['fr_FR', 'be_FR', 'fr_BE', 'fr_LU', 'fr_CH', 'fr_MC']
         qwertz_locales = ['de_DE', 'de_CH', 'de_AT', 'de_LU', 'gsw_DE', 'gsw_CH']
@@ -14,23 +14,27 @@ if platform.system() == "Windows":
         elif current_locale in qwerty_locales:
             return "QWERTY"
         else:
-            raise ValueError(f"Unsupported keyboard layout: {current_locale}")
-
+            return "QWERTY"
 
 
 def relative_path(base_path: str, target_path: str) -> str:
     return os.path.abspath(os.path.join(os.path.dirname(__file__), "..", base_path, target_path))
 
+
 def src_path(target_path: str) -> str:
     return relative_path("src", target_path)
 
+
 def img_path(target_path: str) -> str | None:
-    if (e := relative_path("images", target_path)).split(".")[-1] not in ["png", "jpg", "jpeg", "svg"]: raise ValueError("Invalid image file")
+    if (e := relative_path("images", target_path)).split(".")[-1] not in ["png", "jpg", "jpeg",
+                                                                          "svg"]: raise ValueError("Invalid image file")
     if not os.path.exists(e): return None
     return e
 
+
 def get_animals_path(animal) -> str:
     return img_path(f"Animals/{animal}.png")
+
 
 def get_character_path(character) -> str:
     return img_path(f"Characters/{character}.png")
