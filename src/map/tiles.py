0f5ae72506b5c91fc4e04e53c7d662afacb24ac1
SimonStnn <simon.stijnen.23@gmail.com>
2024-11-15 11:44:24 +0100

    Update tile translations

---
+++ src/map/tiles.py
@@ -0,0 +1,23 @@
+from typing import TypedDict
+from src.functions import img_path
+
+
+class Tile(TypedDict):
+    name: str
+    level: int
+    sprite_url: str
+
+
+def get_tile(name: str, ext: str = ".png", level: int = 1) -> Tile:
+    return {
+        "name": name,
+        "level": level,
+        "sprite_url": img_path(f"Tileset/{name}{ext}") or "Tileset/missing.png"  # type: ignore
+    }
+
+
+TILE_TRANSLATIONS: dict[int, Tile] = {
+    1: get_tile(name="grass"),
+    2: get_tile(name="sand"),
+    3: get_tile(name="water", level=0)
+}
